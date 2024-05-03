9315d1a70ff2d92c016771b74e4153f79286a997
Joaquin Angeles <jqn.ngls@gmail.com>
2024-05-03 14:02:43 -0700

    Created tabs for app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
-import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import DiscoveryScreen from './screens/Planets';
+import HomeScreen from './screens/Home';
+import FavoritesScreen from './screens/Favorites';
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Tab = createBottomTabNavigator();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? '#333' : '#fff',
-  };
-
+const App = (): React.JSX.Element => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <View>
-        <Text>This is the main menu</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        backBehavior="initialRoute"
+        screenOptions={{
+          tabBarActiveTintColor: '#a460fd',
+        }}>
+        <Tab.Screen
+          name="Discovery"
+          component={DiscoveryScreen}
+          options={{
+            tabBarLabel: 'Galaxies',
+          }}
+        />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            tabBarLabel: 'Favorites',
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 export default App;
